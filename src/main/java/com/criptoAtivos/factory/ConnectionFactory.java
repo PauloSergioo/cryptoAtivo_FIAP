7f55663e4004aa9c078cafc2f6ef11957d147aaa
PauloSergioo <paulosergioilha38@gmail.com>
2025-03-16 18:03:58 -0300

    Escondendo meus credenciais

--- src/main/java/com/criptoAtivos/factory/ConnectionFactory.java
+++ src/main/java/com/criptoAtivos/factory/ConnectionFactory.java
@@ -12,8 +12,8 @@ public class ConnectionFactory {
         private static final Logger logger = LoggerFactory.getLogger(ConnectionFactory.class);
 
         private static final String URL = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl";
-        private static final String USUARIO = "rm553012";
-        private static final String SENHA = "110304";
+        private static final String USUARIO = "seuRM";
+        private static final String SENHA = "dataNascEm6Digitos";
 
         public static Connection getConnection() throws SQLException {
                 try {
